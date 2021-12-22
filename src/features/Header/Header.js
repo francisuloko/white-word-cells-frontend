7b83213c16f6e41cb0a99e96ae39f9c8de4f1f18
francisuloko <francisuloko@yahoo.com>
2021-12-23 00:19:24 +0100

    Add header styles

--- src/features/Header/Header.js
+++ src/features/Header/Header.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  Container,
+  Nav,
+  Navbar
+} from "react-bootstrap";
 
 const Header = () => {
   return (
     <>
-      <h1>Word Mango</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/new">Add</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              alt=""
+              src="/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Word Mango
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link to="/" className="btn fs-6 text-white">Home</Link>
+              <Link to="/new" className="btn fs-6 text-white">Add</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 };
