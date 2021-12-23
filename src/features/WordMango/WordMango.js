ebc118bce5262e6e5e0b53538e6fde6e3a946286
francisuloko <francisuloko@yahoo.com>
2021-12-23 22:26:43 +0100

    Position word and story

--- src/features/WordMango/WordMango.js
+++ src/features/WordMango/WordMango.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BoxArrowUpRight } from "react-bootstrap-icons";
 import { useNavigate } from "react-router-dom";
-import { Container, Carousel } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 import "./WordMango.css";
 
 const WordMango = (props) => {
@@ -12,33 +12,28 @@ const WordMango = (props) => {
   };
 
   const wordList = props.wordList.map((obj) => (
-    <Carousel.Item style={{ height: "", backgroundColor: "#000" }}>
-      <img
-        className="w-100"
-        src="https://picsum.photos/200"
-        alt="First slide"
-      />
+    <Carousel.Item className="module mid">
       <Carousel.Caption>
-        <div
+        <h2
           onClick={() => handleEdit(obj)}
           className="d-flex align-items-center"
         >
           <span className="fs-1">{obj.word}</span>
-          <BoxArrowUpRight className=" mx-3 fs-5" />
-        </div>
-        <pre className="story">{obj.story}</pre>
+          <BoxArrowUpRight className=" mx-3 fs-6" />
+        </h2>
+        <pre>{obj.story}</pre>
       </Carousel.Caption>
     </Carousel.Item>
   ));
 
   return (
-    <Container className="">
+    <>
       {wordList.length > 0 ? (
         <Carousel>{wordList}</Carousel>
       ) : (
         <h2 className="">Add a new word</h2>
       )}
-    </Container>
+    </>
   );
 };
 
