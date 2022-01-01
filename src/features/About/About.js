9ec68c21358d57b6d967a9372b7f2fe5b8daf544
francisuloko <francisuloko@yahoo.com>
2022-01-01 15:17:47 +0100

    Add user authentication service

--- src/features/About/About.js
+++ src/features/About/About.js
@@ -4,7 +4,7 @@ const About = () => (
   <div className="container text-center col-12 col-md-6 p-5 mt-5">
     <h2>About</h2>
     <p>
-      White Word Cells is an affirmation store app that helps you stay
+      White Word Cells is an affirmation (motivation, quotes, etc) store app that helps you stay
       motivated when you feel low. Lorem, ipsum dolor sit amet consectetur
       adipisicing elit. Maxime numquam incidunt reiciendis aut nihil totam
       quod possimus quibusdam quaerat nam? Saepe corporis architecto assumenda
---
+++ src/services/auth.service.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/auth/";
+
+const register = (name, email, password) => {
+  return axios.post(API_URL + "signup", {
+    name,
+    email,
+    password,
+  });
+};
+
+const login = (email, password) => {
+  return axios
+    .post(API_URL + "signin", {
+      email,
+      password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+const authService = {
+  register,
+  login,
+  logout,
+};
+
+export default authService;
