41ae91d75de91599977a2a667a79b0fcd586a096
francisuloko <francisuloko@yahoo.com>
2022-01-02 22:47:48 +0100

    Add landing page to app and modify routes attributes

--- src/App.js
+++ src/App.js
@@ -7,50 +7,25 @@ import Header from './features/Header/Header';
 import About from './features/About/About';
 import Login from './features/Login';
 import Register from './features/Register';
-import Profile from './features/Profile';
-import { history } from './helpers/history';
+import history from './helpers/history';
 import LandingPage from './features/LandingPage';
 
 import './App.css';
 
 function App() {
-  // const [list, setList] = useState("");
-
-  // const handleCreate = (word) => {
-  //   setList([...list, word]);
-  // };
-
-  // const handleModify = (list) => {
-  //   list.forEach((obj, index) => ({ ...obj, id: index + 1 }));
-  //   setList(list);
-  // };
-
   return (
     <div className="App">
       <Router history={history}>
         <Header />
         <div>
           <Routes>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Register} />
-            <Route exact path="/profile" component={Profile} />
-            <Route
-              exact
-              path="/new"
-              component={<NewCell addNewWord={handleCreate} list={list} />}
-            />
-            <Route
-              exact
-              path="/edit"
-              component={<EditCell updateWord={handleModify} list={list} />}
-            />
-            <Route exact path="/about" component={<About />} />
-            <Route
-              exact
-              path="/home"
-              component={<Cells wordList={list} updateList={handleModify} />}
-            />
-            <Route exact path="/" component={LandingPage} />
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/signup" exact element={<Register />} />
+            <Route exact path="/new" element={<NewCell />} />
+            <Route exact path="/edit" element={<EditCell />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/home" element={<Cells />} />
+            <Route path="/" exact element={<LandingPage />} />
           </Routes>
         </div>
       </Router>
