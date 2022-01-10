8f7eb3de4a3f088236d012a53c146d6b75edb6da
francisuloko <francisuloko@yahoo.com>
2022-01-10 21:33:34 +0100

    Clean up commented texts

--- src/features/CreateButton/CreateButton.js
+++ src/features/CreateButton/CreateButton.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { PlusLg } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
-// import createIcon from "../../feather.svg";
 
 const CreateButton = () => (
   <div
@@ -17,7 +16,6 @@ const CreateButton = () => (
       to="/new"
       className="d-flex btn p-3 mb-3 text-white fs-5 btn-primary rounded-circle align-items-center"
     >
-      {/* <img src={createIcon} alt="createIcon" style={{width: "18px"}} /> */}
       <PlusLg />
     </Link>
   </div>
