ea7adef6348f5cb1a507dac01e38a9dc6e890744
francisuloko <francisuloko@yahoo.com>
2022-01-19 03:01:51 +0100

    Write about app using less than 300 characters

--- src/features/About/About.js
+++ src/features/About/About.js
@@ -4,14 +4,12 @@ const About = () => (
   <div className="container text-center col-12 col-md-6 p-5">
     <h2>About</h2>
     <p>
-      White Word Cells is a word store app that lets users store
-      words with a custom description.
-
-      It is modelled after the white blood cells which forms
-      part of the body&apos;s immune system to help fight infections and other diseases.
-
-      The aim is for users to store personalized affirmations or motivational
-      quotes to help them stay positive when they feel low.
+      White Word Cells lets you save words with custom descriptions.
+      You can store personalized affirmations
+      or motivations to help you stay motivated
+      or you can reframe words that limit your abilities.
+      The app is modelled after the white blood cells
+      which help the body fight infections and other diseases.
     </p>
   </div>
 );
