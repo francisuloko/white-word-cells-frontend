75dc6474ce89ed1cd2e60243aefced8985fa9750
francisuloko <francisuloko@yahoo.com>
2022-01-19 10:02:45 +0100

    Modify about text

--- src/features/About/About.js
+++ src/features/About/About.js
@@ -4,12 +4,11 @@ const About = () => (
   <div className="container text-center col-12 col-md-6 p-5">
     <h2>About</h2>
     <p>
-      White Word Cells lets you save words with custom descriptions.
-      You can store personalized affirmations
-      or motivations to help you stay motivated
-      or you can reframe words that limit your abilities.
-      The app is modelled after the white blood cells
-      which help the body fight infections and other diseases.
+      White Word Cells lets you save words with custom descriptions. You can
+      store personalized affirmations and motivations or reframe words
+      with bad vibes. The app is modelled after the white blood cells
+      to help you fight negative thoughts. We are made of word, what words are
+      you made of?
     </p>
   </div>
 );
