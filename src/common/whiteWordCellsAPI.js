a257637c73b7ada589c9c27664d984cfa9f05fce
francisuloko <francisuloko@yahoo.com>
2022-01-22 01:51:55 +0100

    Create whiteWordCellsAPI headers

---
+++ src/common/whiteWordCellsAPI.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const whiteWordCellsAPI = axios.create({
+  // baseURL: 'https://whitewordcells.herokuapp.com/api/v1/',
+  baseURL: 'http://localhost:3001/api/v1/',
+  withCredentials: true,
+});
+
+export default whiteWordCellsAPI;
