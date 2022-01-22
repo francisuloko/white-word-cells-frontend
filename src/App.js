3bbd5f69493da366dccf362b9f6ac9715c54b3d2
francisuloko <francisuloko@yahoo.com>
2022-01-22 01:56:24 +0100

    Fetch loggedin user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { loginStatus } from './slices/auth';
 import PrivateRoute from './features/PrivateRoutes';
 import Cells from './features/Cells/Cells';
 import NewCell from './features/NewCell/NewCell';
@@ -13,31 +15,39 @@ import LandingPage from './features/LandingPage';
 import Alert from './features/Alerts/Alert';
 import './App.css';
 
-const App = () => (
-  <div className="App d-flex flex-column vh-100">
-    <Router>
-      <Header />
-      <Alert />
-      <div className="flex-grow-1 position-relative">
-        <Routes>
-          <Route path="/login" exact element={<Login />} />
-          <Route path="/signup" exact element={<Register />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/how-it-works" element={<Instructions />} />
-          <Route path="/" exact element={<LandingPage />} />
-          <Route exact path="/cells" element={<PrivateRoute />}>
-            <Route exact path="/cells" element={<Cells />} />
-          </Route>
-          <Route exact path="/new" element={<PrivateRoute />}>
-            <Route exact path="/new" element={<NewCell />} />
-          </Route>
-          <Route exact path="/edit" element={<PrivateRoute />}>
-            <Route exact path="/edit" element={<EditCell />} />
-          </Route>
-        </Routes>
-      </div>
-    </Router>
-  </div>
-);
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loginStatus());
+  }, []);
+
+  return (
+    <div className="App d-flex flex-column vh-100">
+      <Router>
+        <Header />
+        <Alert />
+        <div className="flex-grow-1 position-relative">
+          <Routes>
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/signup" exact element={<Register />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/how-it-works" element={<Instructions />} />
+            <Route path="/" exact element={<LandingPage />} />
+            <Route exact path="/cells" element={<PrivateRoute />}>
+              <Route exact path="/cells" element={<Cells />} />
+            </Route>
+            <Route exact path="/new" element={<PrivateRoute />}>
+              <Route exact path="/new" element={<NewCell />} />
+            </Route>
+            <Route exact path="/edit" element={<PrivateRoute />}>
+              <Route exact path="/edit" element={<EditCell />} />
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
