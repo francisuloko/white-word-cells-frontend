045d341985ecec73cd1009d3375926fcc1a2744c
francisuloko <francisuloko@yahoo.com>
2022-01-30 18:04:36 +0100

    Modify axios header

--- src/common/whiteWordCellsAPI.js
+++ src/common/whiteWordCellsAPI.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const whiteWordCellsAPI = axios.create({
   baseURL: 'https://whitewordcells.herokuapp.com/api/v1/',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  },
   // baseURL: 'http://localhost:3001/api/v1/',
   withCredentials: true,
 });
